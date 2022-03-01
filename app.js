1224e5d1465ed1ef6f739dce3d41f859f0cc9849
HP 840 <yasin80545859@.com>
2022-03-01 16:29:25 +0600

    saven

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const searchButton = () =>{
 
 // Display Result
 const displayResult = phones =>{
-    const allPhone = phones.data
+    const allPhone = phones.data.slice(0,20)
     const displayCard = document.getElementById('display-card')
     displayCard.textContent ='';
 
@@ -26,17 +26,17 @@ const displayResult = phones =>{
   div.classList.add('col')
   div.classList.add('mb-3')
   div.classList.add('shadow')
-  div.classList.add('p-4')
+  div.classList.add('p-3')
   div.innerHTML=`
-    <div class="card h-100">
-                <img src="${phone.image}" class="card-img-top w-75" alt="...">
+    <div class="card h-100 w-80">
+                <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
                 <div class="card-body">
                   <h3 class="card-title">${phone.phone_name}</h3>
                   <h5 class="card-title fw-bold">${phone.brand}</5>
                   <p class="card-text">${phone.slug}</p>
                 </div>
                 <div class="card-footer bg-white">
-                   <button onclick="detailByClick('${phone.slug}')" class="bg-secondary text-white border border-rounded w-100">See Detail</button>
+                   <button onclick="detailByClick('${phone.slug}')" class="bg-secondary text-white border border-rounded w-100 fw-bold">See Detail</button>
                 </div>
               </div>
         `
@@ -62,16 +62,20 @@ const displaySigleCard = phoneId =>{
   const div = document.createElement('div')
   phoneDetail.innerHTML='';
   div.classList.add('card')
-  div.classList.add('w-25')
+  // div.classList.add('w-50')
   div.classList.add('mb-3')
   div.classList.add('shadow')
   div.classList.add('p-3')
   div.innerHTML =`
-  <img src="${phoneId.image}" class="card-img-top" alt="...">
+  <img src="${phoneId.image}" class="card-img-top w-50 mx-auto" alt="...">
           <div class="card-body">
             <h3 class="card-title">${phoneId.name}</h3>
             <h5 class="card-title">${phoneId.brand}</h5>
-            <p class="card-text">${phoneId.slug}</p>
+            <p class="card-text">${phoneId.releaseDate}</p>
+            <h4 class="card-title text-primary">main features</h4>
+            <p><span class="fw-bold">chipSet:</span> ${phoneId.mainFeatures.chipSet}</p>
+            <p><span class="fw-bold">displaySize:</span> ${phoneId.mainFeatures.displaySize}</p>
+            <p><span class="fw-bold">memory:</span> ${phoneId.mainFeatures.memory}</p>
           </div>
         </div>
   ` 
